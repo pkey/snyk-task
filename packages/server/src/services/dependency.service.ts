bae7cbf47f54becc8a7473a834bcbf5375d9319c
pkey <kutka.paulius@gmail.com>
2019-11-17 17:57:44 +0200

    Implemented dependency fetching

--- packages/server/src/services/dependency.service.ts
+++ packages/server/src/services/dependency.service.ts
@@ -1,11 +1,47 @@
+import axios from "axios";
 import { Dependency } from "../models";
 
 class DependencyService {
+  private maxDepth;
   constructor() {}
 
-  //Default depth is 0
+  //Default depth is set to 0 - if none provided will return just the dependency
   async getDependencies(name, version, depth = 0): Promise<Dependency> {
-    return;
+    this.maxDepth = depth;
+    return await this.getTree(name, version, 0);
+  }
+
+  async getTree(name, version, currentDepth) {
+    const dependency: Dependency = {
+      name: name,
+      version: version,
+      dependencies: []
+    };
+
+    if (currentDepth === this.maxDepth) return dependency;
+
+    const response = await axios.get(
+      `https://registry.npmjs.org/${name}/${version}`
+    );
+
+    const dependencies = {
+      ...response.data.dependencies,
+      ...response.data.devDependencies
+    };
+
+    const dependencyPromises: Promise<Dependency>[] = [];
+
+    for (let name of Object.keys(dependencies)) {
+      dependencyPromises.push(
+        this.getTree(name, dependencies[name], currentDepth + 1)
+      );
+    }
+
+    await Promise.all(dependencyPromises).then((dependencies: any) => {
+      dependency.dependencies = dependencies;
+    });
+
+    return dependency;
   }
 }
 
--- packages/server/test/dependency.service.spec.ts
+++ packages/server/test/dependency.service.spec.ts
@@ -1,11 +1,24 @@
 let chai = require("chai");
+import nock from "nock";
 import dependencyService from "../src/services/dependency.service";
-
 const { expect } = chai;
 
 describe("DependencyService", () => {
+  beforeEach(() => {
+    nock("https://registry.npmjs.org")
+      .get(/\/.*/)
+      .reply(200, {
+        dependencies: {},
+        devDependencies: {}
+      });
+  });
+  afterEach(() => {
+    nock.cleanAll();
+  });
   it("should return a properly formatted dependency", async () => {
-    expect(new dependencyService().getDependencies("dep", "latest")).to.eql({
+    expect(
+      await new dependencyService().getDependencies("dep", "latest")
+    ).to.eql({
       name: "dep",
       version: "latest",
       dependencies: []
@@ -13,17 +26,26 @@ describe("DependencyService", () => {
   });
 
   it("should return both dev and normal dependencies", async () => {
-    expect(new dependencyService().getDependencies("dep", "latest")).to.eql({
+    nock.cleanAll();
+    nock("https://registry.npmjs.org")
+      .get("/dep/latest")
+      .reply(200, {
+        dependencies: { normalDep: "latest" },
+        devDependencies: { devDep: "latest" }
+      });
+    expect(
+      await new dependencyService().getDependencies("dep", "latest", 1)
+    ).to.eql({
       name: "dep",
       version: "latest",
       dependencies: [
         {
-          name: "devDep",
+          name: "normalDep",
           version: "latest",
           dependencies: []
         },
         {
-          name: "normalDep",
+          name: "devDep",
           version: "latest",
           dependencies: []
         }
@@ -31,23 +53,104 @@ describe("DependencyService", () => {
     });
   });
 
+  it("should stop fetching if depth limit is reached", async () => {
+    nock.cleanAll();
+    nock("https://registry.npmjs.org")
+      .get("/dep/latest")
+      .reply(200, {
+        dependencies: { normalDep: "latest" },
+        devDependencies: { devDep: "latest" }
+      });
+    expect(
+      await new dependencyService().getDependencies("dep", "latest", 0)
+    ).to.eql({
+      name: "dep",
+      version: "latest",
+      dependencies: []
+    });
+  });
+
   describe("when depth is set", () => {
-    it("should return nested dependencies and stop fetching more", async () => {
+    beforeEach(() => {
+      //Mock responses from npmjs
+      nock.cleanAll();
+      nock("https://registry.npmjs.org")
+        .get("/dep/latest")
+        .reply(200, {
+          dependencies: { dep1: "latest" },
+          devDependencies: { dep2: "latest" }
+        });
+      nock("https://registry.npmjs.org")
+        .get("/dep1/latest")
+        .reply(200, {
+          dependencies: { dep11: "latest" },
+          devDependencies: { dep12: "latest" }
+        });
+      nock("https://registry.npmjs.org")
+        .get("/dep2/latest")
+        .reply(200, {
+          dependencies: { dep21: "latest" },
+          devDependencies: { dep22: "latest" }
+        });
+    });
+    it("should return nested dependencie (1 level)", async () => {
       expect(
-        new dependencyService().getDependencies("dep", "latest", 1)
+        await new dependencyService().getDependencies("dep", "latest", 1)
       ).to.eql({
         name: "dep",
         version: "latest",
         dependencies: [
           {
-            name: "devDep",
+            name: "dep1",
             version: "latest",
             dependencies: []
           },
           {
-            name: "normalDep",
+            name: "dep2",
             version: "latest",
             dependencies: []
+          }
+        ]
+      });
+    });
+    it("should return nested dependencies (2 levels)", async () => {
+      expect(
+        await new dependencyService().getDependencies("dep", "latest", 2)
+      ).to.eql({
+        name: "dep",
+        version: "latest",
+        dependencies: [
+          {
+            name: "dep1",
+            version: "latest",
+            dependencies: [
+              {
+                name: "dep11",
+                version: "latest",
+                dependencies: []
+              },
+              {
+                name: "dep12",
+                version: "latest",
+                dependencies: []
+              }
+            ]
+          },
+          {
+            name: "dep2",
+            version: "latest",
+            dependencies: [
+              {
+                name: "dep21",
+                version: "latest",
+                dependencies: []
+              },
+              {
+                name: "dep22",
+                version: "latest",
+                dependencies: []
+              }
+            ]
           }
         ]
       });
